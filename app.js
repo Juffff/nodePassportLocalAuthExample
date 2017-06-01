2c4cb5d04e7a6785fceedd3cb5ebec9d60a8d277
Voytsekhivskyi Volodymyr <juff@voytsekskyismbp.corp.muk.ua>
2017-06-01 15:43:29 +0300

    fake ready

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const process = require('process');
 const fs = require('fs');
 const serveStatic = require('serve-static');
 const bodyParser = require('body-parser');
@@ -7,6 +8,8 @@ const util = require('util');
 const expressSession = require('express-session');
 const cookieParser = require('cookie-parser');
 const {passport, checkAuth} = require('./lib/auth/auth');
+const local = require('./lib/templates/local');
+const fake = require('./lib/fake');
 
 
 
@@ -21,20 +24,32 @@ app
     .use(expressSession({secret: 'qqqq'}))
     .use(passport.initialize())
     .use(passport.session())
+
+    .get('/api/1.0.0/users', (req, res) => {
+        res.send(fake({count: req.query.perPage || 20, type:'user'}));
+    })
+
+    .get('/api/1.0.0/users:1', (req, res) => {
+        res.send(fake({count:1, type:'user'}));
+    })
+
     .get('/login', (req, res) => {
         fs.createReadStream('./lib/templates/login.html').pipe(res);
     })
+
     .get('/', checkAuth, (req, res) => {
-        fs.createReadStream('./lib/templates/local.html').pipe(res);
+        res.send(local);
     });
 
-app.post('/login',
+app
+    .post('/login',
     passport.authenticate('local', {
         successRedirect: '/',
         failureRedirect: '/login'
     })
 );
 
-app.listen(3000, () => {
+app
+    .listen(3000, () => {
     console.log('Listening localhost:3000');
 });
